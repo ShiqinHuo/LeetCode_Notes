2f05cefb33c69a2cbb30b0255ad48ab027ed0498
ShiqinHuo <ShiqinHuo@gmail.com>
2018-03-27 23:57:03 +1100

    python debugging reflect for Q.486_medium

--- MiniMax_Practice/486 Predict the Winner/solution.py
+++ MiniMax_Practice/486 Predict the Winner/solution.py
@@ -1,20 +1,21 @@
 # http://bookshadow.com/weblog/2017/01/22/leetcode-predict-the-winner/
 
-class Solution(object):
+class Solution:
     def PredictTheWinner(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        dic = dict()
+        dic = {}
+        # tnums = tuple(nums)
+        # move to the solve function
         def solve(nums):
+            ###BUG！ IndexError: list index out of range
             if len(nums) <=1: return sum(nums)
-            tnums = tuple(nums) ## convert list to tuple
+            tnums = tuple(nums)
             if tnums in dic:
                 return dic[tnums]
             dic[tnums] = max(nums[0]-solve(nums[1:]),nums[-1]-solve(nums[:-1]))
-            # recursion : max of solve()
+            # TypeError: unsupported operand type(s) for -: 'int' and 'NoneType'
             return dic[tnums]
         return solve(nums) >= 0
-
-    # learn by heart
