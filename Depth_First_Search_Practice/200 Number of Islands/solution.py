69a10eae0cbb8b770cef2d337ffadb7e95dd1201
ShiqinHuo <ShiqinHuo@gmail.com>
2018-03-28 21:18:47 +1100

    python solution for Q.200_medium！*****

--- Depth_First_Search_Practice/200 Number of Islands/solution.py
+++ Depth_First_Search_Practice/200 Number of Islands/solution.py
@@ -8,11 +8,10 @@ class Solution:
         else:
             h,w = len(grid),len(grid[0])
 
-
             def dfs(y, x):
-                if 0<=y<h and 0<=x<w and grid[y][x]:
-                    grid[y][x] = 0
+                if 0<=y<h and 0<=x<w and grid[y][x] == "1":
+                    grid[y][x] = "0"
                     return 1 + dfs(y-1,x)+dfs(y+1,x)+dfs(y,x+1)+dfs(y,x-1)
                 return 0
-            areas = [dfs(y,x) for x in range(w) for y in range(h) if grid[y][x] and dfs(y,x)!=0]
+            areas = [dfs(y,x) for x in range(w) for y in range(h) if grid[y][x] == "1" and dfs(y,x)!=0]
             return len(areas)
