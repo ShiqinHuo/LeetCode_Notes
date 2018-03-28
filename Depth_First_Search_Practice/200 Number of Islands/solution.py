ae19d5f80a23184a58f4abc48f49b03f9b2dca78
ShiqinHuo <ShiqinHuo@gmail.com>
2018-03-28 19:25:04 +1100

    RuntimeError for large files - Q.200_medium!

---
+++ Depth_First_Search_Practice/200 Number of Islands/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if grid == []: return 0
+        else:
+            h,w = len(grid),len(grid[0])
+
+
+            def dfs(y, x):
+                if 0<=y<h and 0<=x<w and grid[y][x]:
+                    grid[y][x] = 0
+                    return 1 + dfs(y-1,x)+dfs(y+1,x)+dfs(y,x+1)+dfs(y,x-1)
+                return 0
+            areas = [dfs(y,x) for x in range(w) for y in range(h) if grid[y][x] and dfs(y,x)!=0]
+            return len(areas)
