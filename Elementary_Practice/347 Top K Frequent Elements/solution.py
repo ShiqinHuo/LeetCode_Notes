c56d569f0f1dc0920a6c05adeec0bbce90b9d61f
Shiqin Huo <ShiqinHuo@gmail.com>
2019-01-01 02:44:34 +0800

    Q.347_medium

---
+++ Elementary_Practice/347 Top K Frequent Elements/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = {}
+        temp = []
+        for i in nums:
+            if i in d:
+                d[i] +=1
+            else:
+                d[i] = 1
+
+        output = heapq.nlargest(k, d.iteritems(), operator.itemgetter(1))
+        for k,v in output:
+            temp.append(k)
+        return temp
