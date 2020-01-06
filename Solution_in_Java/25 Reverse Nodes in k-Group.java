4fa03ac502e443ffabfccabb2d1817329146be11
Shiqin Huo <ShiqinHuo@gmail.com>
2020-01-06 20:50:03 +0800

    Create 25 Reverse Nodes in k-Group.java

---
+++ Solution_in_Java/25 Reverse Nodes in k-Group.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        /**
+        For k = 2, you should return: 2->1->4->3->5
+
+        For k = 3, you should return: 3->2->1->4->5
+        */
+        if (head == null || head.next == null) return head;
+        int count = 0;
+        ListNode cur = head;
+        while (cur != null && count != k) {
+            cur = cur.next;
+            count++;
+        }
+        if (count == k) {
+            cur = reverseKGroup(cur, k); // recursion
+            while (count-- > 0) {
+                // reverse the linkedlist
+                ListNode temp = head.next;
+                head.next = cur;
+                cur = head;
+                head = temp;
+            }
+            head = cur;
+        }
+        return head;
+    }
+}
