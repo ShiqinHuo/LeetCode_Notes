92648398618b9b4a370349c9e39774fe72837ed1
Shiqin Huo <ShiqinHuo@gmail.com>
2020-03-24 20:56:04 +0800

    Create 112. Path Sum.java

---
+++ Solution_in_Java/112. Path Sum.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        // leaf + checked !
+        if (root.left == null && root.right == null) return sum == root.val;
+        
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
